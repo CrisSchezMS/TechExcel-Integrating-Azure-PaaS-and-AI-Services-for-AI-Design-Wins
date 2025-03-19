1894023941cce10c66502da445848759d4b977cf
CrisSchezMS <cristinas@microsoft.com>
2025-03-19 10:14:26 +0100

    Update DatabaseService.cs

--- src/ContosoSuitesWebAPI/Services/DatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/DatabaseService.cs
@@ -2,6 +2,8 @@ using System.Runtime.CompilerServices;
 using Microsoft.Data.SqlClient;
 using ContosoSuitesWebAPI.Entities;
 using Azure.Core;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
 
 namespace ContosoSuitesWebAPI.Services;
 
@@ -13,6 +15,10 @@ public class DatabaseService(string connectionString) : IDatabaseService
     /// <summary>
     /// Get all hotels from the database.
     /// </summary>
+    /// 
+    [KernelFunction]
+    [Description("Get all hotels.")]
+    
     public async Task<IEnumerable<Hotel>> GetHotels()
     {
         var sql = "SELECT HotelID, HotelName, City, Country FROM dbo.Hotel";
@@ -41,7 +47,11 @@ public class DatabaseService(string connectionString) : IDatabaseService
     /// <summary>
     /// Get a specific hotel from the database.
     /// </summary>
-    public async Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId)
+    [KernelFunction]
+    [Description("Get all bookings for a single hotel.")]
+    public async Task<IEnumerable<Booking>> GetBookingsForHotel(
+        [Description("The ID of the hotel")] int hotelId
+        )
     {
         var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID";
         using var conn = new SqlConnection(
@@ -72,7 +82,12 @@ public class DatabaseService(string connectionString) : IDatabaseService
     /// <summary>
     /// Get bookings for a specific hotel that are after a specified date.
     /// </summary>
-    public async Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt)
+    [KernelFunction]
+    [Description("Get all bookings for a single hotel and minimum date.")]
+    public async Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(
+        [Description("The ID of the hotel")] int hotelId,
+        [Description("The minimum date for the bookings")] DateTime dt
+        )
     {
         var sql = "SELECT BookingID, CustomerID, HotelID, StayBeginDate, StayEndDate, NumberOfGuests FROM dbo.Booking WHERE HotelID = @HotelID AND StayBeginDate >= @StayBeginDate";
         using var conn = new SqlConnection(
@@ -101,6 +116,8 @@ public class DatabaseService(string connectionString) : IDatabaseService
         return bookings;
     }
 
+    [KernelFunction]
+    [Description("Get all bookings for hotels with missing rooms.")] 
     public async Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms()
     {
         var sql = """
@@ -143,6 +160,9 @@ public class DatabaseService(string connectionString) : IDatabaseService
 
         return bookings;
     }
+    
+    [KernelFunction]
+    [Description("Get all bookings for hotels with multiple rooms.")] 
     public async Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms()
     {
         var sql = """
